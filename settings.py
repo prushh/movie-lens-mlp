6dc98570ad41116cc592ccaa09dfd8ad74f86392
Davide <davide.pruscini@studio.unibo.it>
2022-03-29 03:00:09 +0200

    Create settings.py
    It will contain global variables like paths, useful lists, etc.

---
+++ settings.py
@@ -0,0 +1,11 @@
+DATASETS_URL = 'https://files.grouplens.org/datasets/movielens/ml-latest.zip'
+DATASETS_DIR = 'datasets'
+
+csv_names = [
+    'genome-scores.csv',
+    'genome-tags.csv',
+    'links.csv',
+    'movies.csv',
+    'ratings.csv',
+    'tags.csv'
+]
