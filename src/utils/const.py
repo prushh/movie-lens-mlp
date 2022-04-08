29c991e00c6cff8cf32fc3c06148ad7035689f62
Davide <davide.pruscini@studio.unibo.it>
2022-04-08 18:32:16 +0200

    Added folderpath to const.py

--- src/utils/const.py
+++ src/utils/const.py
@@ -1,3 +1,4 @@
+import os
 from string import Template
 
 MOVIE_LENS_URL = 'https://files.grouplens.org/datasets/movielens/ml-latest.zip'
@@ -5,6 +6,10 @@ TMDB_API_URL = Template('https://api.themoviedb.org/3/movie/${tmdb_id}?api_key=$
 IMDB_URL = 'https://datasets.imdbws.com/'
 
 DATA_DIR = 'data'
+RAW_DIR = os.path.join(DATA_DIR, 'raw')
+EXTERNAL_DIR = os.path.join(DATA_DIR, 'external')
+INTERIM_DIR = os.path.join(DATA_DIR, 'interim')
+PROCESSED_DIR = os.path.join(DATA_DIR, 'processed')
 
 DATA_SUB_DIRS = [
     'external',
