4f6f744d30c6f501acc9fb43290b0954bc9187fe
prushh <davide.pruscini@gmail.com>
2022-06-13 15:43:35 +0200

    Update network hyperparams

--- src/models/config.py
+++ src/models/config.py
@@ -7,20 +7,36 @@ from sklearn.naive_bayes import GaussianNB
 from sklearn.svm import SVC
 from sklearn.tree import DecisionTreeClassifier
 
+# param_layers = {
+#     'input_act': [nn.ReLU(), nn.LeakyReLU(), nn.Tanh()],
+#     'hidden_act': [nn.ReLU(), nn.LeakyReLU()],
+#     'num_hidden_layers': [3, 5, 7],
+#     'dropout': np.arange(0.2, 0.6, .1),
+#     'batch_norm': [False, True],
+#     'output_fn': [None]
+# }
+#
+# param_grid_mlp = {
+#     'batch_size': [64, 128],
+#     'optim': [torch.optim.Adam, torch.optim.SGD],
+#     'momentum': np.arange(0.5, 1, .2),
+#     'weight_decay': [0.00001, 0.000001, 0.0000001]
+# }
+
 param_layers = {
-    'input_act': [nn.ReLU(), nn.LeakyReLU(), nn.Tanh()],
-    'hidden_act': [nn.ReLU(), nn.LeakyReLU()],
-    'num_hidden_layers': [3, 5, 7],
-    'dropout': np.arange(0.2, 0.6, .1),
-    'batch_norm': [False, True],
+    'input_act': [nn.ReLU()],
+    'hidden_act': [nn.LeakyReLU()],
+    'num_hidden_layers': [5],
+    'dropout': [0.2],
+    'batch_norm': [False],
     'output_fn': [None]
 }
 
 param_grid_mlp = {
     'batch_size': [64, 128],
-    'optim': [torch.optim.Adam, torch.optim.SGD],
-    'momentum': np.arange(0.5, 1, .2),
-    'weight_decay': [0.00001, 0.000001, 0.0000001]
+    'optim': [torch.optim.Adam],
+    'momentum': [0.9],
+    'weight_decay': [0.000001]
 }
 
 param_grid_svc = {
