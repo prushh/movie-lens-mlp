6a6413e67ce68bc2537830e090aa73445742a9ef
prushh <davide.pruscini@gmail.com>
2022-07-06 02:52:27 +0200

    Some refactoring to use argparse parameters
    Improve easy training, print info about configuration

--- src/models/sklearn_models.py
+++ src/models/sklearn_models.py
@@ -1,4 +1,5 @@
 import pickle
+from random import randrange
 from typing import Tuple
 
 import numpy as np
@@ -55,7 +56,7 @@ def preprocess(train_data: pd.DataFrame, test_data: pd.DataFrame) -> Tuple:
     return train_data_proc, test_data_proc
 
 
-def fit_model(df: pd.DataFrame, model_group: str):
+def fit_model(df: pd.DataFrame, model_group: str, easy_params: bool, model_to_test: str, test: bool):
     df = (df
           .assign(rating_discrete=pd.cut(df.loc[:, 'rating_mean'], bins=NUM_BINS, labels=False))
           .astype({'rating_discrete': 'int32'})
@@ -64,42 +65,54 @@ def fit_model(df: pd.DataFrame, model_group: str):
     data = df.loc[:, df.columns != 'rating_discrete']
     target = df['rating_discrete']
 
-    outer_results = []
-    cv_outer = StratifiedKFold(n_splits=5, shuffle=True)
+    if not test:
+        outer_results = []
+        cv_outer = StratifiedKFold(n_splits=5, shuffle=True)
 
-    for model_name, estimator, param_grid in param_grid_model[model_group]:
-        for fold, (train_idx, test_idx) in enumerate(cv_outer.split(data, y=target), 1):
-            print(f'Fold {fold}')
-            train_data, test_data = data.iloc[train_idx, :], data.iloc[test_idx, :]
-            train_target, test_target = target[train_idx], target[test_idx]
+        # TODO: now take only one model from a group, reduce also hyperparams
+        if easy_params:
+            random_conf = randrange(len(param_grid_model[model_group]))
+            easy_param_grid = [param_grid_model[model_group][random_conf]]
+            correct_param_grid = easy_param_grid
+        else:
+            correct_param_grid = param_grid_model[model_group]
 
-            cv_inner = StratifiedKFold(n_splits=5, shuffle=True)
+        for model_name, estimator, param_grid in correct_param_grid:
+            for fold, (train_idx, test_idx) in enumerate(cv_outer.split(data, y=target), 1):
+                print(f'Fold {fold}')
+                train_data, test_data = data.iloc[train_idx, :], data.iloc[test_idx, :]
+                train_target, test_target = target[train_idx], target[test_idx]
 
-            train_data_smt, train_target_smt = balance(train_data, train_target)
-            train_data_proc, test_data_proc = preprocess(train_data_smt, test_data)
+                cv_inner = StratifiedKFold(n_splits=5, shuffle=True)
 
-            search = GridSearchCV(estimator=estimator,
-                                  param_grid=param_grid,
-                                  scoring='accuracy',
-                                  cv=cv_inner,
-                                  refit=True,
-                                  n_jobs=-1,
-                                  verbose=3)
+                train_data_smt, train_target_smt = balance(train_data, train_target)
+                train_data_proc, test_data_proc = preprocess(train_data_smt, test_data)
 
-            search.fit(train_data_proc, train_target_smt)
-            best_model = search.best_estimator_
-            y_pred = best_model.predict(test_data_proc)
-            acc = accuracy_score(test_target, y_pred)
-            loss = zero_one_loss(test_target, y_pred)
-            outer_results.append(acc)
+                search = GridSearchCV(estimator=estimator,
+                                      param_grid=param_grid,
+                                      scoring='accuracy',
+                                      cv=cv_inner,
+                                      refit=True,
+                                      n_jobs=-1,
+                                      verbose=3)
 
-            if acc == max(outer_results):
-                filename = f'{model_name}.pkl'
-                pickle.dump(search.best_estimator_, open(filename, 'wb'))
+                search.fit(train_data_proc, train_target_smt)
+                best_model = search.best_estimator_
+                y_pred = best_model.predict(test_data_proc)
+                acc = accuracy_score(test_target, y_pred)
+                loss = zero_one_loss(test_target, y_pred)
+                outer_results.append(acc)
 
-            print(f'loss={loss:3f}, accuracy={acc:3f}, est={search.best_score_:3f}, cfg={search.best_params_}')
+                if acc == max(outer_results):
+                    filename = f'{model_name}.pkl'
+                    pickle.dump(search.best_estimator_, open(filename, 'wb'))
 
-        print(f'[{model_name}] [test] Mean accuracy: {np.mean(outer_results):3f} - σ: {np.std(outer_results):3f}')
+                print(f'loss={loss:3f}, accuracy={acc:3f}, est={search.best_score_:3f}, cfg={search.best_params_}')
+
+            print(f'[{model_name}] [test] Mean accuracy: {np.mean(outer_results):3f} - σ: {np.std(outer_results):3f}')
+    else:
+        # TODO: Use only 20% of dataset?
+        test_eval(model_to_test, data, target)
 
 
 def test_eval(filepath: str, test_data, test_target):
