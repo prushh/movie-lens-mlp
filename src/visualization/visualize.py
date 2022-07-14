7a69185c3c485768e0fcb15e2ad4df48805fa65e
cotus997 <giosue.cotugno@studio.unibo.it>
2022-07-14 20:48:48 +0200

    Balance eval + visualization functions

--- src/visualization/visualize.py
+++ src/visualization/visualize.py
@@ -1,6 +1,7 @@
 import os
 
 import matplotlib.pyplot
+import numpy as np
 import pandas as pd
 from matplotlib import pyplot as plt
 import seaborn as sns
@@ -18,6 +19,61 @@ custom_params = {
 
 
 # sns.set_theme(rc=custom_params)
+
+def barplot_multiple_balancing(df: pd.DataFrame, title: str) -> None:
+    fig, ax = plt.subplots(figsize=(16, 10))
+    scores = ['mean_acc']
+    # X deve essere il range del numero di gruppi di grafico
+    X = np.arange(len(df['model_name'].unique()))
+    width = 0.1
+    rs = [X]
+    # deve scorrere il range del numero di barre che ci sono nel gruppo
+    for idx in range(1, len(df['balance'].unique())):
+        tmp = rs[idx - 1]
+        rs.append(
+            [val + width for val in tmp]
+        )
+    # va creata un'array che contiene un elemento per gruppo di grafico quindi in questo caso
+    # l'elemento avrà cardinalità |len(df['model_name'].unique())|
+    for idx, elm in enumerate(df['balance'].unique()):
+        # df[df['balance']==elm][scores].to_numpy().squeeze() = [f1_random, f1_decision, f1_gaussian, f1_quadratic]
+        ax.bar(x=rs[idx], height=df[df['balance'] == elm][scores].to_numpy().squeeze(), label=elm, width=width)
+
+    ax.legend(loc='lower left')
+    loc_ticks = [(val + (len(df['balance'].unique()) / 2) * width) - width / 2 for val in
+                 range(len(df['model_name'].unique()))]
+    upper_labels = [val.upper() for val in df['model_name'].unique()]
+    ax.set_title(title, fontsize=24)
+    ax.set_xticks(loc_ticks)
+    ax.set_xticklabels(upper_labels)
+
+    plt.show()
+
+
+def barplot_one_balancing(df: pd.DataFrame, title: str) -> None:
+
+    #never used
+    fig, ax = plt.subplots(figsize=(16, 10))
+    scores = ['mean_acc', 'mean_f1']
+    X = np.arange(len(scores))
+    width = 0.2
+    rs = [X]
+    for idx in range(1, df.shape[0]):
+        tmp = rs[idx - 1]
+        rs.append(
+            [val + width for val in tmp]
+        )
+
+    for idx in range(df.shape[0]):
+        ax.bar(rs[idx], df.iloc[idx][scores].to_numpy(), label=df.iloc[idx]['model_name'], width=width)
+    ax.legend(loc='lower left')
+    loc_ticks = [(val + (df.shape[0] / 2) * width) - width / 2 for val in range(len(scores))]
+    upper_labels = [val.upper() for val in scores]
+    ax.set_title(title)
+    ax.set_xticks(loc_ticks)
+    ax.set_xticklabels(upper_labels)
+
+    plt.show()
 
 
 def histplot(x_values: pd.Series, title: str, xlabel: str, ylabel: str, filename: str = '', **kwargs) -> None:
