3588e2d743a15c6389df1a205b75636913556e59
cotus997 <giosue.cotugno@studio.unibo.it>
2022-07-18 21:10:13 +0200

    finished imbalance and added function on visualize

--- src/visualization/visualize.py
+++ src/visualization/visualize.py
@@ -17,12 +17,47 @@ custom_params = {
     'ytick.labelsize': 15
 }
 
-
 # sns.set_theme(rc=custom_params)
+#sns.set_palette('bright')
+
+
+def show_class_distribution(df: pd.DataFrame):
+
+    sns.set_theme(rc=custom_params)
+    sns.set_palette('bright')
+    distribution = (df.reset_index()
+                    .groupby('rating_discrete')
+                    .agg('count')
+                    .reset_index()
+                    .rename(columns={'index': 'count'}))
+
+    # create pie chart
+    plt.gca().axis("equal")
+
+    pie = plt.pie(distribution['count'], startangle=0, pctdistance=0.9, radius=1.2)
+
+    plt.title('Class distribution train set', fontsize=24)
+
+    # Defining intervals labels
+
+    labels = [f'{round(i, 2)}-{round(i + 0.45, 2)}' for i in np.arange(0.5, 5, 0.45)]
+    plt.legend(pie[0],
+               labels,
+               bbox_to_anchor=(0.75, 0.5),
+               loc="center right",
+               fontsize=18,
+               bbox_transform=plt.gcf().transFigure)
+    plt.subplots_adjust(left=0.0, bottom=0.1, right=0.85)
+    plt.show()
+    plt.clf()
+    plt.close()
 
 
 def barplot_multiple_columns(groups: list, elements_group: list, data: list, title: str, filename: str = '',
-                             save: bool = False) -> None:
+                             save: bool = False, label_count: bool = False) -> None:
+    sns.set_theme(rc=custom_params)
+
+    sns.set_palette('bright')
     fig, ax = plt.subplots(figsize=(16, 10))
 
     # X deve essere il range del numero di gruppi di grafico
@@ -40,8 +75,12 @@ def barplot_multiple_columns(groups: list, elements_group: list, data: list, tit
     for idx, elm in enumerate(elements_group):
         # df[df['balance']==elm][scores].to_numpy().squeeze() = [f1_random, f1_decision, f1_gaussian, f1_quadratic]
         ax.bar(x=rs[idx], height=data[idx], label=elm, width=width)
+        if label_count:
+            ax.text(rs[idx], 1.05,
+                    data[idx],
+                    ha='center', va='bottom', rotation=90)
 
-    ax.legend(loc='lower left')
+    ax.legend(fontsize=18, loc='lower left')
     loc_ticks = [(val + (len(elements_group) / 2) * width) - width / 2 for val in
                  range(len(groups))]
     upper_labels = [val.upper() for val in groups]
